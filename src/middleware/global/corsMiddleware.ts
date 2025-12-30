e163ea164fb88bc90e4566399a01ff70c6e72047
gamabuntafrog <kirik.slaw@gmail.com>
2025-12-30 11:49:43 +0200

    fix(cors): update origin handling

--- src/middleware/global/corsMiddleware.ts
+++ src/middleware/global/corsMiddleware.ts
@@ -6,9 +6,9 @@ const allowedOrigins = new Set(config.FRONTEND_ORIGINS);
 export const corsMiddleware = cors({
   origin: (origin) => {
     if (!origin) {
-      return true;
+      return "*";
     }
-    return allowedOrigins.has(origin);
+    return allowedOrigins.has(origin) ? origin : null;
   },
   credentials: true,
   allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
