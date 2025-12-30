ad4041e0cf46bc086d517db4705bb8987983e5b9
gamabuntafrog <kirik.slaw@gmail.com>
2025-12-30 11:16:48 +0200

    chore(commitlint): increase body max line length to 500 characters for commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,6 +25,7 @@ export default {
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 500],
   },
 };
 
